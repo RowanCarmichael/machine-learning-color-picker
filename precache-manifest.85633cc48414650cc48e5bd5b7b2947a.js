159414563f03de0a34ebe42082f780c6662f229d
Rowan Carmichael <carmichaelr@hotmail.co.uk>
2020-10-23 14:47:48 +1100

    Updates

---
+++ precache-manifest.85633cc48414650cc48e5bd5b7b2947a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7254d539687a5d3b311e3e0473941e1",
+    "url": "/machine-learning-color-picker/index.html"
+  },
+  {
+    "revision": "c8eca685e76d2ca98f41",
+    "url": "/machine-learning-color-picker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ac3fb12d4c5e89c4b11f",
+    "url": "/machine-learning-color-picker/static/js/2.adb04c89.chunk.js"
+  },
+  {
+    "revision": "c8eca685e76d2ca98f41",
+    "url": "/machine-learning-color-picker/static/js/main.b594b606.chunk.js"
+  },
+  {
+    "revision": "e7a9e0c4948a64f290f4",
+    "url": "/machine-learning-color-picker/static/js/runtime~main.0645d8d6.js"
+  },
+  {
+    "revision": "34f48416228497c263ca995e8d74a898",
+    "url": "/machine-learning-color-picker/static/media/loading.34f48416.svg"
+  },
+  {
+    "revision": "96bdc12e95e2788d504941c94ca6932c",
+    "url": "/machine-learning-color-picker/static/media/thumb-down.96bdc12e.svg"
+  },
+  {
+    "revision": "cbf4a4aa5a37e2c953e952286a46567a",
+    "url": "/machine-learning-color-picker/static/media/thumb-up.cbf4a4aa.svg"
+  },
+  {
+    "revision": "cb215ae41d53eb3012f4d107cdd94023",
+    "url": "/machine-learning-color-picker/static/media/up-arrow.cb215ae4.svg"
+  }
+]);
